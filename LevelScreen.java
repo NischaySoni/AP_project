9d3f67a519dc852956ac0a05d01ac1956b81730f
SARTHAK <sarthak23490@iiitd.ac.in>
2024-10-18 17:52:37 +0530

    change in level class

--- LevelScreen.java
+++ LevelScreen.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
@@ -9,6 +10,11 @@ import java.awt.event.ComponentEvent;
 
 public class LevelScreen {
     public LevelScreen(){
+//        JLabel label = new JLabel();
+//        label.setBorder(border);
+//        label.setVerticalAlignment(JLabel.CENTER);
+//        label.setHorizontalAlignment(JLabel.CENTER);
+
         JFrame levelscreen = new JFrame("Levels");
         levelscreen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -17,8 +23,33 @@ public class LevelScreen {
         device.setFullScreenWindow(levelscreen);
         levelscreen.setLayout(null);
 
-        BackHomeButton button = new BackHomeButton("Back");
 
+        BackHomeButton button = new BackHomeButton("Back");
+        button.setFont(new Font("Comic Sans", Font.BOLD, 25));
+        button.setForeground(Color.black);
+        button.setBackground(Color.green);
+        button.setBounds(100, 100, 200, 100);
+//        button.setPreferredSize(new Dimension(200, 100));
+        button.setFocusable(false);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("back");
+//                new LevelScreen();
+            }
+        });
+
+
+//        JButton button = new JButton("Back!");
+//        button.setBounds(50, 50, 120, 40); // Set position (x, y) and size (width, height)
+//        button.setFont(new Font("Comic Sans", Font.BOLD, 25));
+//        button.setForeground(Color.black);
+//        button.setBackground(Color.green);
+//        button.setFocusable(false);
+
+
+        levelscreen.add(button);
         levelscreen.setVisible(true);
     }
 }
