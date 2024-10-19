732172d18e47d89dad589cfb00b41fbe5ea3f2a6
NischaySoni <nischay23353@iiitd.ac.in>
2024-10-19 18:49:15 +0530

    levelScreen

--- LevelScreen.java
+++ LevelScreen.java
@@ -28,7 +28,7 @@ public class LevelScreen {
         label.setBorder(border);
         label.setVerticalAlignment(JLabel.CENTER);
         label.setHorizontalAlignment(JLabel.CENTER);
-        label.setLayout(new GridBagLayout()); 
+        label.setLayout(new GridBagLayout());
 
         JButton level1Button = new JButton("Level 1");
         JButton level2Button = new JButton("Level 2");
@@ -59,11 +59,11 @@ public class LevelScreen {
             }
         });
 
-        JButton backButton = new JButton("Back");
+        BackHomeButton backButton = new BackHomeButton("Back");
         backButton.setFont(new Font("Comic Sans", Font.BOLD, 20));
         backButton.setForeground(Color.green);
         backButton.setBackground(Color.black);
-        backButton.setPreferredSize(new Dimension(150, 50)); 
+        backButton.setPreferredSize(new Dimension(150, 50));
         backButton.setFocusable(false);
 
         backButton.addActionListener(new ActionListener() {
@@ -71,14 +71,14 @@ public class LevelScreen {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Back to main screen");
                 pvrframe.setVisible(true);
-                levelscreen.setVisible(false); 
+                levelscreen.setVisible(false);
             }
         });
 
         GridBagConstraints c = new GridBagConstraints();
-        c.gridx = 0; 
+        c.gridx = 0;
         c.gridy = GridBagConstraints.RELATIVE;
-        c.insets = new Insets(10, 10, 10, 10); 
+        c.insets = new Insets(10, 10, 10, 10);
 
         label.add(level1Button, c);
         label.add(level2Button, c);
@@ -89,10 +89,10 @@ public class LevelScreen {
     }
 
     private void customizeLevelButton(JButton button) {
-        button.setFont(new Font("Comic Sans", Font.BOLD, 15)); 
+        button.setFont(new Font("Comic Sans", Font.BOLD, 15));
         button.setForeground(Color.white);
         button.setBackground(Color.blue);
         button.setFocusable(false);
-        button.setPreferredSize(new Dimension(150, 50)); 
+        button.setPreferredSize(new Dimension(150, 50));
     }
 }
