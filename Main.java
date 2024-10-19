7200b82d5d949d2ecba29dbfcc550a6685c172b5
NischaySoni <nischay23353@iiitd.ac.in>
2024-10-19 20:57:12 +0530

    next week diwali break start

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 import java.io.File;
-import  java.io.IOException;
+import java.io.IOException;
 import javax.sound.sampled.*;
+
 public class Main {
-    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         new Screen();
         File file = new File("bgm.wav");
-        AudioInputStream audio = AudioSystem.getAudioInputStream(file);
-        Clip clip = AudioSystem.getClip();
-        clip.open(audio);
-        clip.start();
+        if (file.exists()) {
+            AudioInputStream audio = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.start(); 
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } 
+        else {
+            System.out.println("Audio file not found: " + file.getAbsolutePath());
+        }
     }
 }
