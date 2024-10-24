c34aee32c8add0a2af0f7f27b212b5b81840ca2c
SARTHAK <sarthak23490@iiitd.ac.in>
2024-10-24 17:45:34 +0530

    Pause class

---
+++ core/src/main/java/io/github/ap/Pause.java
@@ -0,0 +1,12 @@
+package io.github.ap;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Pause {
+}
