cbc0a4c5daf30c5fc0c9494886b61c0fae291489
NischaySoni <147941669+NischaySoni@users.noreply.github.com>
2024-11-27 15:48:19 +0530

    Update Level1.java

--- core/src/main/java/io/github/ap/Level1.java
+++ core/src/main/java/io/github/ap/Level1.java
@@ -4,13 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+
 public class Level1 extends Levels {
 
+    public enum BirdType {
+        RED, YELLOW, BLACK, BLUE
+    }
+    private BirdType currentBird = BirdType.RED;
     private boolean isPaused = false;
     private Music music;
     private BitmapFont font;
@@ -18,7 +25,6 @@ public class Level1 extends Levels {
     private BlueBird blueBird;
     private BlackBird blackBird;
     private YellowBird yellowBird;
-    private SlingShot slingShot;
     private KingPig kingPig;
     private Pig1 pig1;
     private Pig2 pig2;
@@ -35,12 +41,34 @@ public class Level1 extends Levels {
     private boolean isPig2 = true;
     private boolean isTNT = true;
 
-
-
-
+    private ShapeRenderer shapeRenderer;
+    private Vector2 startPoint1, endPoint1;
+    private Vector2 startPoint2, endPoint2;
+    private boolean isDragging1, isDragging2;
+    private Texture slingShotTexture;  // Texture for slingshot band
+    private float slingshotX, slingshotY;
 
     public Level1(Main main, SpriteBatch spriteBatch) {
         super(main, spriteBatch);
+        shapeRenderer = new ShapeRenderer();
+        slingShotTexture = new Texture("slingShot.png");
+
+        // Set input processor to handle mouse input for slingshot
+        Gdx.input.setInputProcessor(new Level1.SlingShotInputProcessor());
+
+        // Position for slingshot texture
+        slingshotX = Gdx.graphics.getWidth() / 5f;  // Adjust X to move it to the left (1/5th of the screen width)
+        slingshotY = Gdx.graphics.getHeight() / 4f;  // Adjust Y to move it lower (1/4th of the screen height)
+
+        // Calculate the top position of the slingshot texture
+        float slingshotTopY = slingshotY + slingShotTexture.getHeight() / 2f;  // Top of the texture
+
+        // Initialize the start and end points of the slingshot lines to the top of the slingshot
+        startPoint1 = new Vector2(slingshotX, slingshotTopY); // Slingshot anchor point 1 (top of texture)
+        endPoint1 = new Vector2(startPoint1); // Initially the same as start
+
+        startPoint2 = new Vector2(slingshotX + slingShotTexture.getWidth(), slingshotTopY); // Slingshot anchor point 2 (right side of texture, top)
+        endPoint2 = new Vector2(startPoint2); // Initially the same as start
         font = new BitmapFont();
         font.setColor(Color.WHITE);
         font.getData().setScale(3);
@@ -74,15 +102,83 @@ public class Level1 extends Levels {
     @Override
     public void show() {}
 
+    private void drawStaticSlingshot() {
+        // Draw the static slingshot image at the given position
+        spriteBatch.begin();
+        spriteBatch.draw(slingShotTexture, slingshotX - slingShotTexture.getWidth() / 2, slingshotY - slingShotTexture.getHeight() / 2);
+        spriteBatch.end();
+    }
+
+    private void drawSlingshotLines() {
+        shapeRenderer.setProjectionMatrix(main.viewport.getCamera().combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+
+        // Draw the first line if dragging
+        if (isDragging1) {
+            shapeRenderer.line(startPoint1.x, startPoint1.y, endPoint1.x, endPoint1.y);
+        }
+
+        // Draw the second line if dragging
+        if (isDragging2) {
+            shapeRenderer.line(startPoint2.x, startPoint2.y, endPoint2.x, endPoint2.y);
+        }
+
+        shapeRenderer.end();
+    }
+
+    private void drawSlingshotTextures() {
+        spriteBatch.begin();
+
+        // Draw first slingshot band texture (from startPoint1 to endPoint1)
+        if (isDragging1) {
+            float angle1 = (float) Math.atan2(endPoint1.y - startPoint1.y, endPoint1.x - startPoint1.x); // Angle for rotation
+            float distance1 = startPoint1.dst(endPoint1); // Distance between the points (length of the band)
+
+            // Draw the first texture stretched along the angle, applying rotation and scaling
+            spriteBatch.draw(slingShotTexture, startPoint1.x, startPoint1.y,
+                0, slingShotTexture.getHeight() / 2,
+                distance1, slingShotTexture.getHeight(),
+                1, 1
+            );
+        }
+
+        // Draw second slingshot band texture (from startPoint2 to endPoint2)
+        if (isDragging2) {
+            float angle2 = (float) Math.atan2(endPoint2.y - startPoint2.y, endPoint2.x - startPoint2.x); // Angle for rotation
+            float distance2 = startPoint2.dst(endPoint2); // Distance between the points (length of the band)
+
+            // Draw the second texture stretched along the angle, applying rotation and scaling
+            spriteBatch.draw(slingShotTexture, startPoint2.x, startPoint2.y,
+                0, slingShotTexture.getHeight() / 2,
+                distance2, slingShotTexture.getHeight(),
+                1, 1
+            );
+        }
+
+        spriteBatch.end();
+    }
+
+
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
 
         spriteBatch.begin();
+        spriteBatch.draw(levelTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        spriteBatch.end();
 
         if (isPaused) {
             return;
         }
+
+        // Draw the static slingshot texture (this is your stationary slingshot band)
+        drawStaticSlingshot();
+
+        // Draw the slingshot lines using ShapeRenderer
+        drawSlingshotLines();
+
+        // Draw the dynamic slingshot textures (bands) on top of the static one
+        drawSlingshotTextures();
 
         if (redBird.isLaunched()) {
             redBird.update(delta);
@@ -130,69 +226,44 @@ public class Level1 extends Levels {
             }
         }
         if (blueBird.isLaunched()) {
-            //BlueBirdFragment blueBirdFragment = new BlueBirdFragment(blueBird.getX(), blueBird.getY(), 500, 500);
             blueBird.update(delta);
-            spriteBatch.draw(bluesTexture, blueBird.getX(), blueBird.getY(), 40, 40);  // Draw BlueBird
-
-            for (BlueBirdFragment fragment : blueBird.getFragments()) {
-                spriteBatch.draw(bluesTexture, fragment.getX(), fragment.getY(), 40, 40);  // Draw fragments
-            boolean isCollisionDetected = false;  // Flag to track if any collision happens
-                // Check for collision with King Pig
-                if (isKingPig && fragment.isColliding(kingPig.getX(), kingPig.getY(), kingPig.getWidth(), kingPig.getHeight())) {
-                    System.out.println("Blue Bird hit the King Pig!");
-                    kingPig.takeDamage();
-                    handleCollision(fragment, kingPig);
-                    isCollisionDetected = true;  // Mark that a collision has occurred
-                }
-                if (isPig1 && blueBird.checkCollision(pig1.getX(), pig1.getY(), pig1.getWidth(), pig1.getHeight())) {
-                    System.out.println("Blue Bird hit the King Pig!");
-                    pig1.takeDamage();
-                    handleCollision(blueBird, pig1);
-                    blueBird.reset();
-                }
-                if (isPig2 && blueBird.checkCollision(pig2.getX(), pig2.getY(), pig2.getWidth(), pig2.getHeight())) {
-                    System.out.println("Blue Bird hit the King Pig!");
-                    pig2.takeDamage();
-                    handleCollision(blueBird, pig2);
-                    blueBird.reset();
-                }
-
-                // Check for collision with Wood
-                if (isWood && fragment.isColliding(wood.getX(), wood.getY(), wood.getWidth(), wood.getHeight())) {
-                    System.out.println("Blue Bird hit the Wood!");
-                    wood.takeDamage();
-                    handleCollision(fragment, wood);
-                    isCollisionDetected = true;
-                }
-
-                // Check for collision with Glass
-                if (isGlass && fragment.isColliding(glass.getX(), glass.getY(), glass.getWidth(), glass.getHeight())) {
-                    System.out.println("Blue Bird hit the Glass!");
-                    glass.takeDamage();
-                    glass.takeDamage();  // Double damage for glass
-                    handleCollision(fragment, glass);
-                    isCollisionDetected = true;
-                }
-
-                // Check for collision with Stone
-                if (isStone && fragment.isColliding(stone.getX(), stone.getY(), stone.getWidth(), stone.getHeight())) {
-                    System.out.println("Blue Bird hit the Stone!");
-                    stone.takeDamage();
-                    handleCollision(fragment, stone);
-                    isCollisionDetected = true;
-                }
-
-                // Check for collision with TNT
-                if (isTNT && fragment.isColliding(tnt.getX(), tnt.getY(), tnt.getWidth(), tnt.getHeight())) {
-                    System.out.println("Blue Bird hit the TNT!");
-                    tnt.takeDamage();
-                    handleCollision(fragment, tnt);
-                    isCollisionDetected = true;
-                }
-                // Reset BlueBird if any collision occurred
-                if (isCollisionDetected) {
-                    blueBird.reset();
-                }
+            if (isKingPig && blueBird.checkCollision(kingPig.getX(), kingPig.getY(), kingPig.getWidth(), kingPig.getHeight())) {
+                System.out.println("Blue Bird hit the King Pig!");
+                kingPig.takeDamage();
+                handleCollision(blueBird, kingPig);
+            }
+            if (isPig1 && blueBird.checkCollision(pig1.getX(), pig1.getY(), pig1.getWidth(), pig1.getHeight())) {
+                System.out.println("Blue Bird hit the King Pig!");
+                pig1.takeDamage();
+                handleCollision(blueBird, pig1);
+                blueBird.reset();
+            }
+            if (isPig2 && blueBird.checkCollision(pig2.getX(), pig2.getY(), pig2.getWidth(), pig2.getHeight())) {
+                System.out.println("Blue Bird hit the King Pig!");
+                pig2.takeDamage();
+                handleCollision(blueBird, pig2);
+                blueBird.reset();
+            }
+            if (isWood && blueBird.checkCollision(wood.getX(), wood.getY(), wood.getWidth(), wood.getHeight())) {
+                System.out.println("Blue Bird hit the Wood!");
+                wood.takeDamage();
+                handleCollision(blueBird, wood);
+            }
+            if (isGlass && blueBird.checkCollision(glass.getX(), glass.getY(), glass.getWidth(), glass.getHeight())) {
+                System.out.println("Blue Bird hit the Glass!");
+                glass.takeDamage();
+                glass.takeDamage();
+                handleCollision(blueBird, glass);
+            }
+            if (isStone && blueBird.checkCollision(stone.getX(), stone.getY(), stone.getWidth(), stone.getHeight())) {
+                System.out.println("Blue Bird hit the Stone!");
+                stone.takeDamage();
+                handleCollision(blueBird, stone);
+            }
+            if (isTNT && blueBird.checkCollision(tnt.getX(), tnt.getY(), tnt.getWidth(), tnt.getHeight())) {
+                System.out.println("Blue Bird hit the TNT!");
+                tnt.takeDamage();
+                handleCollision(blueBird, tnt);
             }
         }
 
@@ -296,8 +367,7 @@ public class Level1 extends Levels {
             }
         }
 
-        spriteBatch.draw(levelTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        spriteBatch.draw(slingShotTexture, 400, 200, 150, 300);
+        spriteBatch.begin();
         spriteBatch.draw(redTexture, redBird.getX(), redBird.getY(), 100, 100);
         spriteBatch.draw(chuckTexture, yellowBird.getX(), yellowBird.getY(), 100, 100);
         spriteBatch.draw(bluesTexture, blueBird.getX(), blueBird.getY(), 70, 70);
@@ -438,6 +508,111 @@ public class Level1 extends Levels {
         bird.reset();
     }
 
+    // InputProcessor for handling the slingshot dragging behavior
+    private class SlingShotInputProcessor implements com.badlogic.gdx.InputProcessor {
+        @Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            if (button == 0) {
+                // Convert screen coordinates to world coordinates
+                Vector2 mousePos = new Vector2(screenX, Gdx.graphics.getHeight() - screenY); // Invert Y axis
+
+                // Start dragging both lines
+                isDragging1 = true;
+                isDragging2 = true;
+
+                // Update the end points to where the mouse is clicked
+                endPoint1.set(mousePos);
+                endPoint2.set(mousePos);
+            }
+            return true;
+        }
+
+        @Override
+        public boolean touchDragged(int screenX, int screenY, int pointer) {
+            Vector2 mousePos = new Vector2(screenX, Gdx.graphics.getHeight() - screenY); // Invert Y axis
+
+            // If dragging the first line, update its end point
+            if (isDragging1) {
+                endPoint1.set(mousePos);
+            }
+
+            // If dragging the second line, update its end point
+            if (isDragging2) {
+                endPoint2.set(mousePos);
+            }
+
+            return true;
+        }
+
+        @Override
+        public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+            // Stop dragging the first or second line when the left mouse button is released
+            if (button == 0) {
+                isDragging1 = false;
+                isDragging2 = false;
+
+                // Launch the bird based on the drag distance
+                Vector2 dragVector = new Vector2(endPoint1.x - startPoint1.x, endPoint1.y - startPoint1.y);
+                float velocityX = dragVector.x * 2;  // Adjust multiplier for speed
+                float velocityY = dragVector.y * 2;  // Adjust multiplier for speed
+
+                // Launch the selected bird
+                switch (currentBird) {
+                    case RED:
+                        redBird.launch(velocityX, velocityY);
+                        break;
+                    case YELLOW:
+                        yellowBird.launch(velocityX, velocityY);
+                        break;
+                    case BLACK:
+                        blackBird.launch(velocityX, velocityY);
+                        break;
+                    case BLUE:
+                        blueBird.launch(velocityX, velocityY);
+                        break;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public boolean touchCancelled(int i, int i1, int i2, int i3) {
+            return false;
+        }
+
+        // Unused InputProcessor methods (but they must be implemented)
+        @Override
+        public boolean keyDown(int keycode) {
+            switch (keycode) {
+                case Input.Keys.NUM_1:
+                    currentBird = BirdType.RED;
+                    break;
+                case Input.Keys.NUM_2:
+                    currentBird = BirdType.YELLOW;
+                    break;
+                case Input.Keys.NUM_3:
+                    currentBird = BirdType.BLACK;
+                    break;
+                case Input.Keys.NUM_4:
+                    currentBird = BirdType.BLUE;
+                    break;
+            }
+            return true;
+        }
+
+        @Override
+        public boolean keyUp(int keycode) { return false; }
+
+        @Override
+        public boolean keyTyped(char character) { return false; }
+
+        @Override
+        public boolean mouseMoved(int screenX, int screenY) { return false; }
+
+        @Override
+        public boolean scrolled(float amountX, float amountY) { return false; }
+    }
+
 
     private void input() {
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
@@ -458,18 +633,18 @@ public class Level1 extends Levels {
                     main.setScreen(pauseMenu);
                 }
             }
-            if (!redBird.isLaunched() && mouseX >= redBird.getX() && mouseX <= redBird.getX() + 100 && mouseY >= redBird.getY() && mouseY <= redBird.getY() + 100) {
-                redBird.launch();
-            }
-            if (!blueBird.isLaunched() && mouseX >= blueBird.getX() && mouseX <= blueBird.getX() + 100 && mouseY >= blueBird.getY() && mouseY <= blueBird.getY() + 100) {
-                blueBird.launch();
-            }
-            if (!blackBird.isLaunched() && mouseX >= blackBird.getX() && mouseX <= blackBird.getX() + 100 && mouseY >= blackBird.getY() && mouseY <= blackBird.getY() + 100) {
-                blackBird.launch();
-            }
-            if (!yellowBird.isLaunched() && mouseX >= yellowBird.getX() && mouseX <= yellowBird.getX() + 100 && mouseY >= yellowBird.getY() && mouseY <= yellowBird.getY() + 100) {
-                yellowBird.launch();
-            }
+//            if (!redBird.isLaunched() && mouseX >= redBird.getX() && mouseX <= redBird.getX() + 100 && mouseY >= redBird.getY() && mouseY <= redBird.getY() + 100) {
+//                redBird.launch();
+//            }
+//            if (!blueBird.isLaunched() && mouseX >= blueBird.getX() && mouseX <= blueBird.getX() + 100 && mouseY >= blueBird.getY() && mouseY <= blueBird.getY() + 100) {
+//                blueBird.launch();
+//            }
+//            if (!blackBird.isLaunched() && mouseX >= blackBird.getX() && mouseX <= blackBird.getX() + 100 && mouseY >= blackBird.getY() && mouseY <= blackBird.getY() + 100) {
+//                blackBird.launch();
+//            }
+//            if (!yellowBird.isLaunched() && mouseX >= yellowBird.getX() && mouseX <= yellowBird.getX() + 100 && mouseY >= yellowBird.getY() && mouseY <= yellowBird.getY() + 100) {
+//                yellowBird.launch();
+//            }
         }
     }
 
